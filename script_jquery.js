10700ae03b8ec1b152eaa8b8cd7964f2d7541cc1
Joyce Mendonça <joycemendonca2307@gmail.com>
2020-04-13 12:49:27 -0300

    first commit

---
+++ script_jquery.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('button').click(function(){
+        $('#unico').css("color", "red");
+    });
+        
+});
+
+$(function(){
+    $('#azul').click(function(){
+        $('p').css("color", "blue")
+    });
+    $('#vermelho').click(function(){
+        $('p').css("color", "red");
+    });
+});
